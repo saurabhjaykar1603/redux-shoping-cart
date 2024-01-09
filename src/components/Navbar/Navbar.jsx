207441d2a8dbc72c0b421ca51a722e5f6140f083
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-01-09 15:56:03 +0530

    ref : navbar css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,15 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
-      <div className="nav py-4 bg-blue-400 flex justify-center gap-x-10 shadow-lg font-seminold" >
-        <Link to="/">HOME</Link>
-        <Link to="/cart">CART</Link>
-        <Link to="/about">ABOUT</Link>
-        <span>Cart Items : 0</span>
+      <div className="nav py-4 bg-blue-300 flex justify-between shadow-lg font-seminold px-10 sticky top-0">
+        <div className="font-bold">REDUX STORE        </div>
+        <div className=" flex justify-center gap-x-10">
+          {" "}
+          <Link to="/" className="font-semibold">HOME</Link>
+          <Link to="/cart" className="font-semibold">CART</Link>
+          <Link to="/about" className="font-semibold">ABOUT</Link>
+          <span>Cart Items : 0</span>
+        </div>
       </div>
     </>
   );
