b0303b07adff661f23391f8d7d2f5c3f557bedfd
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-02-15 12:09:08 +0530

    ref : responsive cart page

--- src/views/Cart/Cart.jsx
+++ src/views/Cart/Cart.jsx
@@ -10,40 +10,40 @@ function Cart() {
   return (
     <div>
       <h1 className="text-3xl text-center mt-5">cart</h1>
-      <div className="cart-wraper px-36 mt-5">
+      <div className="cart-wraper lg:px-36 xs:px-10 mt-5">
         {products.map((product) => {
           const { image, price, title, id } = product;
           return (
-            <>
-              <div
-                key={id}
-                className="cart-cart h-22 bg-white px-4 py-3 mb-3 rounded-md"
-                style={{ boxShadow: "2px 2px 4px rgba(0,0,0,0.4)" }}
-              >
-                <div className="body flex justify-between items-center ">
-                  <div className="w-20 h-20">
-                    <img src={image} alt="" className="h-20" />
-                  </div>
-                  <div className="w-52">
-                    {" "}
-                    <h4 className="font-bold ">{title}</h4>
-                  </div>
-                  <div>
-                    <h4>Rs {price}</h4>
-                  </div>
-                  <button
-                    onClick={() => {
-                      handleRemove(id);
-                    }}
-                    type="button"
-                    className="px-5 bg-blue-500 text-white rounded-md  py-1 font-bold"
-                  >
-                    {" "}
-                    remove
-                  </button>
+            <div
+              key={id}
+              className="cart-cart h-22 lg:bg-white px-4 py-3 mb-3 rounded-md md:bg-red-400"
+              style={{ boxShadow: "2px 2px 4px rgba(0,0,0,0.4)" }}
+            >
+              <div className="body flex lg:flex-row flex-col justify-between items-center">
+                <div className="w-20 h-20 mb-2 lg:mb-0">
+                  <img
+                    src={image}
+                    alt=""
+                    className="h-20 w-full object-cover"
+                  />
                 </div>
+                <div className="w-full lg:w-52 lg:ml-4">
+                  <h4 className="font-bold text-lg lg:text-xl">{title}</h4>
+                </div>
+                <div className="text-lg lg:text-xl">
+                  <h4>Rs {price}</h4>
+                </div>
+                <button
+                  onClick={() => {
+                    handleRemove(id);
+                  }}
+                  type="button"
+                  className="px-3 lg:px-5 bg-blue-500 text-white rounded-md py-1 font-bold mt-2 lg:mt-0"
+                >
+                  remove
+                </button>
               </div>
-            </>
+            </div>
           );
         })}
       </div>
